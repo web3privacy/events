8d4b9a83447e8bb2556cacc0f7d6f3c5b1a8befd
tree <tree@hidden.cz>
2023-10-11 22:01:00 +0200

    update

--- index.js
+++ index.js
@@ -2,11 +2,20 @@ import Ajv from "npm:ajv@8.8.2";
 import addFormats from "npm:ajv-formats@2.1.1";
 import { load as yamlLoad } from "https://deno.land/x/js_yaml_port@3.14.0/js-yaml.js";
 import { emptyDir } from "https://deno.land/std@0.173.0/fs/mod.ts";
+import { Eta } from "https://deno.land/x/eta@v3.0.3/src/index.ts"
 
 const types = ["summits", "meetups"];
 
 async function _loadYaml(fn) {
   return yamlLoad(await Deno.readTextFile(fn));
+}
+
+function getFlagEmoji(countryCode) {
+  const codePoints = countryCode
+    .toUpperCase()
+    .split('')
+    .map(char =>  127397 + char.charCodeAt());
+  return String.fromCodePoint(...codePoints);
 }
 
 async function test() {
@@ -34,7 +43,13 @@ async function build() {
   await emptyDir("./dist");
   const fn = "./dist/index.json";
   await Deno.writeTextFile(fn, JSON.stringify(output, null, 2));
-  console.log(`File saved: `);
+  console.log(`File saved: `, fn);
+
+  const readmeFn = "./README.md"
+  const eta = new Eta({ views: "./" })
+  console.log(output)
+  await Deno.writeTextFile(readmeFn, eta.render("./README.tpl.eta", { events: output, getFlagEmoji }))
+  console.log(`File saved: `, readmeFn);
 }
 
 switch (Deno.args[0] || "test") {
